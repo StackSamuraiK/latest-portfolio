0ba59962a35eb187a83a0d128e2bb4f1ab10cf2a
StackSamuraiK <kshitizsrivastav3344@gmail.com>
2025-10-13 05:22:12 +0530

    modified-her-section

--- components/Spotlight-new.tsx
+++ components/Spotlight-new.tsx
@@ -19,9 +19,15 @@ export function SpotlightPreview() {
         fill="white"
       />
 
+
       <div className="relative z-10 col-span-1 md:col-span-1 flex flex-col items-start justify-center p-4 pl-8 md:pl-16 pt-20 md:pt-0">
         <div className="text-6xl">
-        <LayoutTextFlipDemo/>
+          <div>
+            <button className="group relative inline-flex items-center text-lg gap-2 rounded-full border border-cyan-600 bg-cyan-950 px-6 py-3 text-sm font-medium text-white transition-all duration-300 hover:bg-cyan-800 hover:border-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-500">
+              Full-Stack Innovator & AI Explorer
+            </button>
+          </div>
+          <LayoutTextFlipDemo />
         </div>
         <p className="mt-4 max-w-lg text-left text-xl font-normal text-neutral-300">
           Engineering future-ready platforms with precision, performance, and purpose.
@@ -29,13 +35,13 @@ export function SpotlightPreview() {
         </p>
         <div className="pt-8 flex flex-wrap gap-4 sm:gap-6">
           <a href="https://www.linkedin.com/in/kshitiz-prakash-srivastava-a9b797256/" target="_blank">
-          <Button2
-          
-          className="bg-blue-600 hover:bg-blue-700 border-blue-600 hover:border-blue-700 text-white"
-          >
-            Let's Connect
-          </Button2>
-            </a>
+            <Button2
+
+              className="bg-blue-600 hover:bg-blue-700 border-blue-600 hover:border-blue-700 text-white"
+            >
+              Let's Connect
+            </Button2>
+          </a>
           <a target="_blank"
             rel="noopener noreferrer"
             href="https://drive.google.com/file/d/1x__b7KUlDRL8Z45Yk9f9Yrkv3XNjup5d/view?usp=sharing">
