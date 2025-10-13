dcd433d775260ec3b2c50a5d5da566b54cf663d7
StackSamuraiK <kshitizsrivastav3344@gmail.com>
2025-10-13 08:00:19 +0530

    fixed-font-ofhero-page

--- components/TextFlip.tsx
+++ components/TextFlip.tsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 
 export function LayoutTextFlipDemo() {
   return (
-    <div className="text-6xl">
+    <div className="text-4xl">
       <motion.div className="relative mx-4 my-4 flex flex-col items-center justify-center gap-4 text-center sm:mx-0 sm:mb-0 sm:flex-row">
         <LayoutTextFlip
           text="Hi, I'm"
