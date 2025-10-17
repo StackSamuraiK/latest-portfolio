0aac3e2b4accd517707930d308f3008bb9a2247a
StackSamuraiK <kshitizsrivastav3344@gmail.com>
2025-10-17 07:44:14 +0530

    added speed insights from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -25,6 +26,7 @@ export default function RootLayout({
         className={`${poppins.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
